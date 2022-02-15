3c1730cbe5f19e130779c8a6d63b0080e63b0cda
optimisedu <80532381+optimisedu@users.noreply.github.com>
2022-02-15 18:06:36 +0000

    Create optical.js
    Optical illusion

---
+++ optical.js
@@ -0,0 +1,40 @@
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = []; //local scoped points
+    const count = 50;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1); // -iterates a math range of 0.{count} x
+        const v = count <= 1 ? 0.5 : y / (count - 1); // -iterates a math range of 0.{count} y
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+  const points = createGrid(); // global scope - different points constant
+  const margin = 100;
+  return ({ context, width, height }) => {
+    context.fillStyle = "#fff";
+    // 0x, 0y
+    context.fillRect(0, 0, width, height);
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u); //lerp = linear interpolation, center the grid - min, max, t
+      const y = lerp(margin, width - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 50, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 15;
+      context.stroke();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
